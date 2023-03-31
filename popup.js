8e6aacbcde4903d7c62393fee8431e07c566a5e1
solomonbarayev <sbarayev2@gmail.com>
2023-03-31 11:58:36 +0300

    adjsut style

--- popup.js
+++ popup.js
@@ -1,20 +1,3 @@
-// //code to add event listener to button click
-
-// chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//   var url = tabs[0].url;
-//   var regex = /proof\.html/;
-//   if (regex.test(url)) {
-//     url = url.replace(regex, '');
-//   }
-//   var copyTextarea = document.createElement('textarea');
-//   document.body.appendChild(copyTextarea);
-//   copyTextarea.value = url;
-//   copyTextarea.select();
-//   document.execCommand('copy');
-//   document.body.removeChild(copyTextarea);
-//   window.close();
-// });
-
 document.getElementById('copy').addEventListener('click', function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var url = tabs[0].url;
