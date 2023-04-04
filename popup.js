be19c416bc78d9f2626704e9c593bf7877f2b4ac
סולומון ברייב <solomonb@ordaprint.local>
2023-04-04 15:21:27 +0300

    update plugin to remove everything after proof.html

--- popup.js
+++ popup.js
@@ -2,12 +2,14 @@ document.getElementById('copy').addEventListener('click', function () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var url = tabs[0].url;
     var regex = /proof\.html/;
+    var splitUrl = url.split('proof.html');
+    var path = splitUrl[0];
     if (regex.test(url)) {
-      url = url.replace(regex, '');
+      url = path;
     }
     var copyTextarea = document.createElement('textarea');
     document.body.appendChild(copyTextarea);
-    copyTextarea.value = url;
+    copyTextarea.value = path;
     console.log(copyTextarea.value);
     copyTextarea.select();
     document.execCommand('copy');
